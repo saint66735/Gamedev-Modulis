c6b7e812c72ec02b5adc993c0ccca59babb26718
GColde <gantasgan@gmail.com>
2021-02-03 13:38:53 +0200

    Terrain ir Folderiai

---
+++ Gamedev Modulis/Assets/Scripts/Valdymas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Valdymas : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAJud = Input.GetAxis("Horizontal"); // Ajud - Ašies judėjimas
+        float zAJud = Input.GetAxis("Vertical");
+
+        // Velocity duoda pastumima (jega) ir nustatau palei kurias asis valdysiu. Time.deltaTime 
+        rb.velocity = new Vector3(xAJud*speed, rb.velocity.y, zAJud*speed)* Time.deltaTime;
+    }
+}
