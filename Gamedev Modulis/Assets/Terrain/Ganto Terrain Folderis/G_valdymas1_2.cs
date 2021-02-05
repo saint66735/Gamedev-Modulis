800b4ea537afab52fc56de95d057ea9b6fa2ee6a
GColde <gantasgan@gmail.com>
2021-02-05 13:46:06 +0200

    .

--- Gamedev Modulis/Assets/Terrain/Ganto Terrain Folderis/G_valdymas1_2.cs
+++ Gamedev Modulis/Assets/Terrain/Ganto Terrain Folderis/G_valdymas1_2.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class G_valdymas1_2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Movement
     public float speed = 20f;
     public float Sensitivity = 100;
     public GameObject Body;
     Rigidbody rb;
     float xRot = 0;
+    float yRot = 0;
 
     // Jumping
     public float jumpForce = 100;
@@ -18,9 +19,7 @@ public class G_valdymas1_2 : MonoBehaviour
     public LayerMask groundMask;
     public Transform groundCheck;
     bool isjumping;
-    // taisymas rb.veloc
     private Vector3 jumping;
-    public float dragCoficientas = 1f;
 
     void Start()
     {
@@ -37,48 +36,38 @@ public class G_valdymas1_2 : MonoBehaviour
         xRot -= mouseY;
         xRot = Mathf.Clamp(xRot, -90f, 90f);
 
+        //yRot += mouseX;
+        //yRot = Mathf.Clamp(yRot, -90, 90f);// cmlap.(kokia rotacija, minverte, maxverte)
+
         float moveFw = Input.GetAxis("Vertical");
         float moveSide = Input.GetAxis("Horizontal");
 
         isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, groundMask);
 
+        // Handmade Drag.
         if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
             if (rb.velocity.magnitude < 5)
                 rb.velocity += Body.transform.right * moveSide * speed + Body.transform.forward * moveFw * speed;
 
-
-        else if (rb.velocity.magnitude >= 0.5 && isGrounded == true)            
+            else if (rb.velocity.magnitude >= 0.5 && isGrounded == true)
                 rb.velocity -= (Body.transform.right * moveSide * speed + Body.transform.forward * moveFw * speed) * 0.01f;
-            
-        
 
-
-
-        Body.transform.Rotate(Vector3.up * mouseX);
-        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        //Rotation
+        Body.transform.Rotate(Vector3.up * mouseX); // palei kamera juda kunas
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f); //tik kamera juda
+        //transform.localRotation = Quaternion.Euler(xRot, yRot, 0f); //tik kamera juda
 
         if (Input.GetButton("Jump") && isGrounded)
         {
             jump();
             isjumping = true;
         }
-        else { 
-            
+
+        else
             isjumping = false;
 
-
-        }
-
         if (!isGrounded)
-        {
             jumping = new Vector3(0, -5, 0);
-        }
-
-
-
-        //Vector3 curVel = rb.velocity;
-        //Vector3 xzVel = new Vector3(curVel.x, 0, curVel.z);
-        //rb.velocity -= xzVel * dragCoficientas;
 
     }
 
@@ -88,11 +77,7 @@ public class G_valdymas1_2 : MonoBehaviour
         rb.AddForce(Vector3.up * jumpForce);
     }
 
-    void FixedUpdate()
-    {
 
-
-    }
 
     
 }
