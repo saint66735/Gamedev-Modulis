90219c11329b1b211584a2fe91a76240f7b5353a
GColde <gantasgan@gmail.com>
2021-03-14 02:43:10 +0200

    HeadBobV2 && menuCameraBreath

---
+++ Gamedev Modulis/Assets/Scripts/Gantas/BreathingEffect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathingEffect : MonoBehaviour
+{
+
+    Vector3 startPos;
+    public float amplitude = 10f;
+    public float period = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float theta = Time.timeSinceLevelLoad / period;
+        float distance = amplitude * Mathf.Sin(theta);
+        transform.localPosition = startPos + Vector3.up * distance;
+
+    }
+}
--- Gamedev Modulis/Assets/Scripts/Gantas/HeadBob.cs
+++ Gamedev Modulis/Assets/Scripts/Gantas/HeadBob.cs
@@ -2,19 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeadBob : MonoBehaviour
+public class HeadBob : MonoBehaviour    
 {
+    
 
+    //Breathing
+    private bool isBreath = true;
+    public float MinHigh = 0.8f;
+    public float MaxHigh = 0.9f;
+
+    //Walking
     private float timer = 0.0f;
     public float bobbingSpeed = 0.18f;
     public float bobbingAmount = 0.2f;
-    public float midpoint = 2.0f;
+    public float midpoint = 1f;
+
+    void Start()
+    {
+
+    }
+
 
     void Update()
     {
-        float waveslice = 0.0f;
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0)
+        {
+            breathingBob();
+        }
+
+        else
+        {
+            walkingBob(horizontal, vertical);
+        }
+    }
+
+    public void breathingBob() 
+    {
+        //Breathing
+        if (isBreath)
+        {
+            midpoint = Mathf.Lerp(midpoint, MaxHigh, Time.deltaTime * 1f);
+            transform.localPosition = new Vector3(transform.localPosition.x, midpoint, transform.localPosition.z);
+            if (midpoint >= MaxHigh - 0.01f)
+            {
+                isBreath = !isBreath;
+            }
+
+        }
+        else
+        {
+            midpoint = Mathf.Lerp(midpoint, MinHigh, Time.deltaTime * 1f);
+            transform.localPosition = new Vector3(transform.localPosition.x, midpoint, transform.localPosition.z);
+            if (midpoint <= MinHigh + 0.01f)
+            {
+                isBreath = !isBreath;
+            }
+        }
+    }
+
+    public void walkingBob(float horizontal, float vertical) 
+    {
+        float waveslice = 0.0f;
 
         Vector3 cSharpConversion = transform.localPosition;
 
@@ -22,6 +74,7 @@ public class HeadBob : MonoBehaviour
         {
             timer = 0.0f;
         }
+
         else
         {
             waveslice = Mathf.Sin(timer);
@@ -46,5 +99,7 @@ public class HeadBob : MonoBehaviour
 
         transform.localPosition = cSharpConversion;
     }
+
+
 
 }
