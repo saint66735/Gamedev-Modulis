d7586b83a9759a013ccb169c487b5693f0db9917
Raviolliox <dominykass2000@gmail.com>
2021-05-09 16:33:31 +0300

    Enemy is alive

--- Gamedev Modulis/Assets/Scripts/Dominykas/EnemyController.cs
+++ Gamedev Modulis/Assets/Scripts/Dominykas/EnemyController.cs
@@ -5,19 +5,20 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    public float lookRadius = 10f;
-
+    public float lookRadius = 15f;
+    public float attackRange = 1f;
     NavMeshAgent agent;
-
     PlayerManager manager;
-
-
     bool playerLocated = false;
+    Animator anima;
     // Start is called before the first frame update
     void Start()
     {
+        
         manager = PlayerManager.FindObjectOfType<PlayerManager>();
         agent = GetComponent<NavMeshAgent>();
+        agent.speed = 5;
+        anima = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -26,8 +27,25 @@ public class EnemyController : MonoBehaviour
         float distance = Vector3.Distance(manager.player.transform.position, transform.position);
         if(distance <= lookRadius)
         {
+            anima.SetBool("PlayerDetected", true);
             agent.SetDestination(manager.player.transform.position);
         }
+        else
+        {
+            anima.SetBool("PlayerDetected", false);
+        }
+
+        if(distance <= attackRange)
+        {
+            agent.speed = 0;
+            anima.SetBool("PlayerNear", true);                        
+        }
+        else
+        {
+            agent.speed = 5f;
+            anima.SetBool("PlayerNear", false);
+        }
+                
     }
 
 
